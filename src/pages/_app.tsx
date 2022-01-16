2fa1fcd4a643e31c9a26e9905fe2c35c0153ef35
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2022-01-16 13:08:52 +0900

    update:adobe fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import adobeLoader from '../functions/adobeloader'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(()=>{
+    if(process.browser){
+      adobeLoader(document);
+    }
+  },[])
   return (
     <Layout>
       <Component {...pageProps} />
