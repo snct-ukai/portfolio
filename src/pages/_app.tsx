b359bdda6e07a380b7e7039a9b66dfb2406814ed
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2022-01-16 19:21:45 +0900

    add:pathname fork

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,19 @@ import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import adobeLoader from '../functions/adobeloader'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   useEffect(()=>{
     if(process.browser){
       adobeLoader(document);
     }
-  },[])
+  },[router])
+
   return (
-    <Layout>
+    <Layout route={router.pathname}>
       <Component {...pageProps} />
     </Layout>
   )
