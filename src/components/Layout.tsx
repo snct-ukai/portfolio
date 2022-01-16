e06eaff1fc640204eea23b9af91a7b780816c760
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2022-01-17 00:02:05 +0900

    update:humburger menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,34 +1,119 @@
 import Link from "next/link"
 import styles from "../styles/base.module.scss"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
+import { getWindowSize } from "../functions/hooks/GetWindowSize";
 
 type Props = {
+  route?: string,
   children?: ReactNode
 }
 
-const Layout = ({children} : Props) =>{
+const Layout = ({route, children} : Props) =>{
+  const aboutstyle = `${styles.LinksChil}` + ` ${route =="/about" ? styles.borderline : {}}`
+  const worksstyle = `${styles.LinksChil}` + ` ${route =="/works" ? styles.borderline : {}}`
+  const skillsstyle = `${styles.LinksChil}` + ` ${route =="/skills" ? styles.borderline : {}}`
+  const blogstyle = `${styles.LinksChil}` + ` ${route =="/blog" ? styles.borderline : {}}`
+  const { width } = getWindowSize();
+  const [isOpen, setIsOpen] = useState(false);
+  const menuFunction = () => setIsOpen(!isOpen);
+
   return(
-    <div>
-      <header className={styles.header}>
-        <div className={styles.headerTitle}>
-          Ukai Shota
+    <div className={styles.base}>
+      <header>
+        <div className={styles.header}>
+          <div className={styles.headerTitle} onClick={() => setIsOpen(false)}>
+            <Link href="/">
+              <a><h1>Ukai Shota</h1></a>
+            </Link>
+          </div>
+          {width>960 ? (
+            <div className={styles.headerLinks}>
+              <div className={aboutstyle}>
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </div>
+              <div className={worksstyle}>
+                <Link href="/works">
+                  <a>Works</a>
+                </Link>
+              </div>
+              <div className={skillsstyle}>
+                <Link href="/skills">
+                  <a>Skills</a>
+                </Link>
+              </div>
+              <div className={blogstyle}>
+                <Link href="/blog">
+                  <a>Blog</a>
+                </Link>
+              </div>
+            </div>) : (
+            <div className={styles.humburger}>
+              <div className={`${!isOpen ? styles.open : styles.close}`} onClick={() => menuFunction()}>
+                <span></span>
+                <span></span>
+                <p>{isOpen ? "Close" : "Menu"}</p>
+              </div>
+            </div>
+          )}
         </div>
-        <div className={styles.headerLinks}>
-          <div className={styles.LinksChil}>
-            About
-          </div>
-          <div className={styles.LinksChil}>
-            Works
-          </div>
-          <div className={styles.LinksChil}>
-            Skills
-          </div>
+      </header>{
+      (isOpen&&width<=960)&&(
+      <div className={`${styles.drawerMenu} ${isOpen ? styles.open : undefined}`}>
+        <div className={styles.footerLinks}>
+            <div className={aboutstyle} onClick={() => setIsOpen(false)}>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </div>
+            <div className={worksstyle} onClick={() => setIsOpen(false)}>
+              <Link href="/works">
+                <a>Works</a>
+              </Link>
+            </div>
+            <div className={skillsstyle} onClick={() => setIsOpen(false)}>
+              <Link href="/skills">
+                <a>Skills</a>
+              </Link>
+            </div>
+            <div className={blogstyle} onClick={() => setIsOpen(false)}>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </div>
         </div>
-      </header>
+      </div>
+      )}
       <div className={styles.main}>
         {children}
       </div>
       <footer className={styles.footer}>
+        <div className={styles.footerLinks}>
+            <div className={aboutstyle}>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </div>
+            <div className={worksstyle}>
+              <Link href="/works">
+                <a>Works</a>
+              </Link>
+            </div>
+            <div className={skillsstyle}>
+              <Link href="/skills">
+                <a>Skills</a>
+              </Link>
+            </div>
+            <div className={blogstyle}>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </div>
+        </div>
+        <div className={styles.copyright}>
+          <small>&copy;2022 Ukai Shota</small>
+        </div>
       </footer>
     </div>
   )
