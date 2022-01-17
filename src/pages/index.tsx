d336b0ad6a8624fd1da6daaf1de4c440adea5395
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2022-01-17 15:54:06 +0900

    create:main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from '../components/Head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import { useRouter } from 'next/router'
 import { getMainPage } from '../functions/functions'
+import { Text } from "../components/Text"
 
 type Props = {
   JPname : string,
-  Enname : string,
+  ENname : string,
   Text : string
 }
 
@@ -15,9 +15,49 @@ const Home = (props : Props) => {
   const router = useRouter();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head/>
-      <main className={styles.main}>
+      <main>
+        <div className={styles.main}>
+          <div className={styles.myName}>
+            <div className={styles.Image}>
+              <img src={"/icon/icon.png"} alt="icon"/>
+            </div>
+            <div className={styles.Name}>
+              <div className={styles.JPname}>
+                <p>{props.JPname}</p>
+              </div>
+              <div className={styles.ENname}>
+                <p>({props.ENname})</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.Text}>
+            <Text text={props.Text}/>
+          </div>
+          <div className={styles.Links}>
+            <div className={styles.LinkIcon}>
+              <a href="https://github.com/snct-ukai" target="_blank" rel="noopener noreferrer">
+                <img src={"/LinkIcon/GitHub.png"} alt="GitHub"/>
+              </a>
+            </div>
+            <div className={styles.LinkIcon}>
+              <a href="https://twitter.com/ukai_0417" target="_blank" rel="noopener noreferrer">
+                <img src={"/LinkIcon/twitter.svg"} alt="twitter"/>
+              </a>
+            </div>
+            <div className={styles.LinkIcon}>
+              <a href="https://www.instagram.com/ukai_0417" target="_blank" rel="noopener noreferrer">
+                <img src={"/LinkIcon/Instagram.png"} alt="Instagram"/>
+              </a>
+            </div>
+            <div className={styles.LinkIcon}>
+              <a href="https://qiita.com/snct-ukai" target="_blank" rel="noopener noreferrer">
+                <img src={"/LinkIcon/qiita.png"} alt="qiita"/>
+              </a>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
