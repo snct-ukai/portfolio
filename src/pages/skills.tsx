fb49db548ea67f69414d4ea233a0625f02183e13
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-02-27 14:41:11 +0900

    update: base layout

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from "next";
 import { getSkills } from "../functions/functions";
 import Head from "../components/Head"
 import { skill_work } from "../classes/skill-work";
+import styles from "../styles/skills.module.scss"
+import { Text } from "../components/Text"
 
 type Props = {
   num : number,
@@ -11,16 +13,34 @@ type Props = {
 }
 
 const skills = (props : Props) => {
-  const works : skill_work[] = []
+  const skills : skill_work[] = []
   for(let i = 0; i < props.num; i++){
-    works.push(new skill_work("skill", props.titles[i], props.texts[i], props.pic_paths[i]));
+    skills.push(new skill_work("skill", props.titles[i], props.texts[i], props.pic_paths[i]));
   }
 
   return(
     <div>
       <Head/>
       <main>
-
+          <div className={styles.pageName}>
+            <p>Skills</p>
+          </div>
+          <div className={styles.container}>
+            {
+              skills.map(element => {
+                return (
+                  <div key="" className={styles.child}>
+                    <div className={styles.pic}>
+                      <img src={`/data/skills/${element.getPic_path()}`} alt={element.getTitle()}/>
+                    </div>
+                    <div className={styles.detail}>
+                      <Text text={element.getTitle()}/>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
       </main>
     </div>
   )
