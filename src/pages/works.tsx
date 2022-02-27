10b73da29ccca5415941f9c580e86f56e3496797
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-02-27 12:41:20 +0900

    update: define getStatisProps

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,10 +1,43 @@
-import { NextPage } from "next";
-import DefaultErrorPage from 'next/error';
+import { GetStaticProps } from "next";
+import { getWorks } from "../functions/functions";
+import Head from "../components/Head"
+import { skill_work } from "../classes/skill-work";
 
-const works : NextPage = () => {
+type Props = {
+  num : number,
+  titles : string[],
+  texts : string[],
+  pic_paths : string[],
+}
+
+const works = (props : Props) => {
+  const works : skill_work[] = []
+  for(let i = 0; i < props.num; i++){
+    works.push(new skill_work("work", props.titles[i], props.texts[i], props.pic_paths[i]));
+  }
+
   return(
-    <DefaultErrorPage statusCode={404}/>
+    <div>
+      <Head/>
+      <main>
+
+      </main>
+    </div>
   )
 }
 
 export default works;
+
+export const getStaticProps : GetStaticProps = async() => {
+  const getprops = await getWorks();
+
+  return {
+    props: {
+      num : getprops.length,
+      titles : getprops.map(value => { return value.getTitle(); }),
+      texts : getprops.map(value => { return value.getText(); }),
+      pic_paths : getprops.map(value => { return value.getPic_path(); })
+    },
+    revalidate: 300,
+  }
+}
