ff101fd46b7e62b721977e1350e780205f5f9ee3
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-03-01 22:31:30 +0900

    update: OGP

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -8,6 +8,13 @@ const head = (props :{title?: string}) => {
       <meta name="description" content="This site is portfolio of Ukai Shota" />
       <meta name="viewport" content="width=device-width"/>
       <link rel="icon" href="/favicon.ico" />
+      
+      <meta property="og:url" content="https://portfolio.snct-ukai.tk" />
+      <meta property="og:type" content=" portfolio" />
+      <meta property="og:title" content=" Ukai Shota" />
+      <meta property="og:description" content=" portfolio site created with nextJS" />
+      <meta property="og:site_name" content="Ukai Shota" />
+      <meta property="og:image" content="https://portfolio.snct-ukai.tk/icon/icon.webp" />
     </Head>
   )
 }
